9f16f5be94ae5db5d41c2bc92a8ef613e1de06fa
ghost9933 <mhatrenikhil36@gmail.com>
2025-04-26 20:50:18 -0500

    goal mark complete enhancement

--- server/routes/goalProgress.js
+++ server/routes/goalProgress.js
@@ -3,6 +3,7 @@ import express from "express";
 import {
   getGoalWithProgress,
   addProgressToGoal,
+  markGoalCompleted 
 } from "../controllers/goalProgressController.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
@@ -13,5 +14,7 @@ router.get("/:goalId", verifyToken, getGoalWithProgress);
 
 // POST /api/goals/:goalId/progress
 router.post("/:goalId/progress", verifyToken, addProgressToGoal);
+
+router.patch("/:goalId/complete", verifyToken, markGoalCompleted);
 
 export default router;
